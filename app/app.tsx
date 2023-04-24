ef61aa7b1b1074fe6735498a0d897510889df7d0
DienNv <dien@ideabox.com.au>
2023-04-24 22:09:43 +0700

    Init ignite

---
+++ app/app.tsx
@@ -0,0 +1,68 @@
+/**
+ * Welcome to the main entry point of the app. In this file, we'll
+ * be kicking off our app.
+ *
+ * Most of this file is boilerplate and you shouldn't need to modify
+ * it very often. But take some time to look through and understand
+ * what is going on here.
+ *
+ * The app navigation resides in ./app/navigators, so head over there
+ * if you're interested in adding screens and navigators.
+ */
+import "./i18n"
+import "./utils/ignoreWarnings"
+import React from "react"
+import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
+import { useInitialRootStore } from "./models"
+import { AppNavigator, useNavigationPersistence } from "./navigators"
+import * as storage from "./utils/storage"
+
+// Set up Reactotron, which is a free desktop app for inspecting and debugging
+// React Native apps. Learn more here: https://github.com/infinitered/reactotron
+export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
+
+// Web linking configuration
+interface AppProps {
+}
+
+/**
+ * This is the root component of our app.
+ */
+function App(props: AppProps) {
+  const hideSplashScreen = null
+  const {
+    initialNavigationState,
+    onNavigationStateChange,
+    isRestored: isNavigationStateRestored,
+  } = useNavigationPersistence(storage, NAVIGATION_PERSISTENCE_KEY)
+
+  const {rehydrated} = useInitialRootStore(() => {
+    // This runs after the root store has been initialized and rehydrated.
+
+    // If your initialization scripts run very fast, it's good to show the splash screen for just a bit longer to prevent flicker.
+    // Slightly delaying splash screen hiding for better UX; can be customized or removed as needed,
+    // Note: (vanilla Android) The splash-screen will not appear if you launch your app via the terminal or Android Studio. Kill the app and launch it normally by tapping on the launcher icon. https://stackoverflow.com/a/69831106
+    // Note: (vanilla iOS) You might notice the splash-screen logo change size. This happens in debug/development mode. Try building the app for release.
+    if (hideSplashScreen) setTimeout(hideSplashScreen, 500)
+  })
+
+  // Before we show the app, we have to wait for our state to be ready.
+  // In the meantime, don't render anything. This will be the background
+  // color set in native by rootView's background color.
+  // In iOS: application:didFinishLaunchingWithOptions:
+  // In Android: https://stackoverflow.com/a/45838109/204044
+  // You can replace with your own loading component if you wish.
+  if (!rehydrated || !isNavigationStateRestored) return null
+
+  // otherwise, we're ready to render the app
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <AppNavigator
+        initialState={initialNavigationState}
+        onStateChange={onNavigationStateChange}
+      />
+    </SafeAreaProvider>
+  )
+}
+
+export default App
---
+++ app/screens/MainScreen/MainScreen.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { myColors } from './styles/Colors';
+import { ThemeContext } from './context/ThemeContext';
+import AwKeyboard from './components/AwKeyboard';
+
+
+
+export default function MainScreen() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <AwKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+});
